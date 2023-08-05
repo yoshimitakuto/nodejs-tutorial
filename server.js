504c4af7d0bfa956ae31c6fc5699d6e703c7b255
Yoshimi Takuto <yocchantaku0127@gmail.com>
2023-08-05 18:04:24 +0900

    nodejs lesson finish

--- server.js
+++ server.js
@@ -3,11 +3,13 @@
 // installしたhttpをrequire（読み込み）をする必要がある
 const http = require('http');
 const PORT = 8000;
+// [fs]ファイルを読み込むモジュール(readFileSync=非同期で読み込み)
+const html = require('fs').readFileSync("./index.html")
 
 // Webサーバーを作る
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.write("Hello World");
+    res.write(html);
     res.end();
 });
 
