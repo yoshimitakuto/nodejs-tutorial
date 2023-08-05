e9a6dd95381a2c4dcbf01b2c3f8a2f6901619178
Yoshimi Takuto <yocchantaku0127@gmail.com>
2023-08-05 17:52:19 +0900

    add runserver function

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log('nodejs');
+// console.log('nodejs');
+
+// installしたhttpをrequire（読み込み）をする必要がある
+const http = require('http');
+const PORT = 8000;
+
+// Webサーバーを作る
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("Hello World");
+    res.end();
+});
+
+// ローカルサーバーを起動
+server.listen(PORT, () => {
+    console.log('server running!');
+});
